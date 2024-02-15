19d50568a57edfcda7eadbf187df0a8958abe513
Trung Tran <trung0246@gmail.com>
2024-02-14 22:07:38 -0800

    feat: Update launch.ps1 script to use main.py as the starting point for running the Python folder. Also, add a link to a Stack Overflow post for understanding credential setup with Git for Windows.

--- launch.ps1
+++ launch.ps1
@@ -1,3 +1,6 @@
-# Run the current python folder using ./server/code/base/__init__.py as beginning
+# Run the current python folder using ./server/code/base/main.py as beginning
+
+# Credential setup
+# https://stackoverflow.com/questions/38333752/trying-to-understand-wincred-with-git-for-windows-confused
 
 python .\server\code\base\main.py
