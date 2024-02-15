b1c20bda1a27cf79020f9a40d85f32817016750d
Trung Tran <trung0246@gmail.com>
2024-02-14 22:11:00 -0800

    feat: Update submodule paths and import twikit module

--- memba/server/code/base/main.py
+++ memba/server/code/base/main.py
@@ -1,5 +1,8 @@
 __import__("sys").path.insert(0, str(__import__("pathlib").Path(__file__).parent.parent.parent.parent.parent.absolute()))
+__import__("sys").path.insert(0, str(__import__("pathlib").Path(__file__).parent.parent / "lib"))
 
 import memba.server.code.base.base as memba_base
+import twikit
 
 print(dir(memba_base))
+print(dir(twikit))
