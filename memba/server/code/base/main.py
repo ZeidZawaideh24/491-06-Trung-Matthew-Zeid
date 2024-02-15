1a1c562a4d028e49214a8d81f6309358499548b6
Trung Tran <trung0246@gmail.com>
2024-02-14 22:41:19 -0800

    feat: Update .gitignore and main.py, add fake_useragent to requirements.txt

--- memba/server/code/base/main.py
+++ memba/server/code/base/main.py
@@ -1,8 +1,13 @@
-__import__("sys").path.insert(0, str(__import__("pathlib").Path(__file__).parent.parent.parent.parent.parent.absolute()))
-__import__("sys").path.insert(0, str(__import__("pathlib").Path(__file__).parent.parent / "lib"))
+(lambda: exec("""
+import sys
+import pathlib
+temp_path = pathlib.Path(__file__).parent.parent
+sys.path.insert(0, str(temp_path.parent.parent.parent.absolute()))
+sys.path.insert(0, str(temp_path / "lib"))
+"""))()
 
 import memba.server.code.base.base as memba_base
-import twikit
+from twikit import twikit
 
 print(dir(memba_base))
 print(dir(twikit))
