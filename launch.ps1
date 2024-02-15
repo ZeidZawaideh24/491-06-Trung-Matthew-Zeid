1a0d3c866601a008e181218b25c45b35f1a3680b
Trung Tran <trung0246@gmail.com>
2024-02-14 22:15:32 -0800

    feat: Update submodule paths and fetch submodule

--- launch.ps1
+++ launch.ps1
@@ -3,4 +3,7 @@
 # Credential setup
 # https://stackoverflow.com/questions/38333752/trying-to-understand-wincred-with-git-for-windows-confused
 
+# Fetch submodule
+git submodule update --init --recursive
+
 python .\server\code\base\main.py
