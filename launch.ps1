e2da052887f7c044bd9ad2211960646cb5d8d2d8
Trung Tran <trung0246@gmail.com>
2024-02-14 22:44:04 -0800

    feat: Update launch.ps1 script with new GitHub repository URL

--- launch.ps1
+++ launch.ps1
@@ -1,4 +1,4 @@
-# Run the current python folder using ./server/code/base/main.py as beginning
+# https://github.com/ZeidZawaideh24/491-06-Trung-Matthew-Zeid
 
 # Credential setup
 # https://stackoverflow.com/questions/38333752/trying-to-understand-wincred-with-git-for-windows-confused
@@ -6,4 +6,5 @@
 # Fetch submodule
 git submodule update --init --recursive
 
+# Run the current python folder using ./server/code/base/main.py as beginning
 python .\server\code\base\main.py
