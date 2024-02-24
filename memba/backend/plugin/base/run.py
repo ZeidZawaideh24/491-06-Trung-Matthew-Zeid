f596de00b2433968d352694d6dc53a453979b190
Trung Tran <trung0246@gmail.com>
2024-02-24 00:40:40 -0800

    feat: Add docstring to serve function

--- memba/backend/plugin/base/run.py
+++ memba/backend/plugin/base/run.py
@@ -1,5 +1,10 @@
 PLUGIN_DB = {}
 
+"""
+:param flag: 0 - tool, 1 - import, 2 - export, 3 - both
+:param ver: plugin version
+:param evt: event handler functions
+"""
 async def serve(flag=0, ver="", evt={}):
 	pass
 
